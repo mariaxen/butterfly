a74848b025c1d1d307c0bed2a0666941c5fac803
mxenoc <9497850+mariaxen@users.noreply.github.com>
2020-04-01 19:14:29 -0700

    New pipeline

--- src/butterfly/Models.py
+++ src/butterfly/Models.py
@@ -43,7 +43,7 @@ def split_sequence(sequence, n_steps):
 		y.append(seq_y)
 	return array(X), array(y)
 
-def CNN(X, y, groups, pixels, features, folds, epochs, optimiser, loss):
+def CNN(X, y, groups, pixels, features, folds, epochs, optimiser, loss, type_model):
     
     results = []
     
@@ -60,29 +60,33 @@ def CNN(X, y, groups, pixels, features, folds, epochs, optimiser, loss):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
-        #Create your CNN
-    #    model = Sequential()
-    #    model.add(Conv1D(32,(3), input_shape=(pixels, pixels)))
-    #    model.add(Activation('relu'))
-    #    model.add(MaxPooling1D(pool_size=(2)))
-    #    model.add(Flatten())
-    #    model.add(Dense(50))
-    #    model.add(Dropout(0.2))
-    #    model.add(Activation('relu'))
-    #    model.add(Dense(features))
-    #    model.add(Activation( 'sigmoid'))
-    #    model.compile(optimizer='adam', loss='mse')
+        #Train your multioutput RF
+        if type_model == 'daisy_chain':
+            model = butterfly.RF.MultiOutputRF(100).fit(X_train, y_train)
+        
+        elif type_model == 'multi_RF_regressor':
+            model = MultiOutputRegressor(RandomForestRegressor(n_estimators=100,
+                                                          min_samples_split = 5,
+                                                          random_state=0))
+            model.fit(X_train, y_train)
+        
+        elif type_model == 'RF_regressor':
+            model = RandomForestRegressor(n_estimators=100,
+                                                          min_samples_split = 5,
+                                                          random_state=0)
+            model.fit(X_train, y_train)
     
-        model = Sequential()
-        model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(pixels, pixels)))
-        model.add(MaxPooling1D(pool_size=2))
-        model.add(Flatten())
-        model.add(Dense(50, activation='relu'))
-        model.add(Dense(features))
-        model.compile(optimizer=optimiser, loss=loss)
-#        model.compile(loss=losses.mean_absolute_error, optimizer='sgd')
+        elif type_model == 'CNN':
 
-        model.fit(X_train, y_train, epochs=epochs, verbose=0)
+            model = Sequential()
+            model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(pixels, pixels)))
+            model.add(MaxPooling1D(pool_size=2))
+            model.add(Flatten())
+            model.add(Dense(50, activation='relu'))
+            model.add(Dense(features))
+            model.compile(optimizer=optimiser, loss=loss)
+
+            model.fit(X_train, y_train, epochs=epochs, verbose=0)
 
     # demonstrate prediction
         y_pred_train = model.predict(X_train, verbose = 0)
@@ -159,53 +163,3 @@ def multi_CNN(X, y, groups, pixels, features, folds, optimiser, loss):
         y_observed_test.append(y_test)
     
     return pd.concat(y_prediction_train), pd.concat(y_observed_train), pd.concat(y_prediction_test),pd.concat(y_observed_test)
-
-def RF(X, y, groups, folds, typeRF):
-    
-    results = []
-    
-    y_prediction_train = []
-    y_observed_train = []
-
-    y_prediction_test = []
-    y_observed_test = []
-    
-    group_kfold = GroupKFold(n_splits=folds)
-    
-    for train_index, test_index in group_kfold.split(X, y, groups):
-
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-
-        #Train your multioutput RF
-        if typeRF == 'daisy_chain':
-            forest_model = butterfly.RF.MultiOutputRF(100).fit(X_train, y_train)
-        
-        elif typeRF == 'multi_RF_regressor':
-            forest_model = MultiOutputRegressor(RandomForestRegressor(n_estimators=100,
-                                                          min_samples_split = 5,
-                                                          random_state=0))
-            forest_model.fit(X_train, y_train)
-        
-        elif typeRF == 'RF_regressor':
-            forest_model = RandomForestRegressor(n_estimators=100,
-                                                          min_samples_split = 5,
-                                                          random_state=0)
-            forest_model.fit(X_train, y_train)
-        
-        # demonstrate prediction
-        y_pred_train = forest_model.predict(X_train)
-        y_pred_train = pd.DataFrame(y_pred_train)
-
-        y_pred_test = forest_model.predict(X_test)
-        y_pred_test = pd.DataFrame(y_pred_test)
-        
-        y_train = pd.DataFrame(y_train)
-        y_test  = pd.DataFrame(y_test)
-        
-        y_prediction_train.append(y_pred_train)
-        y_observed_train.append(y_train)
-        y_prediction_test.append(y_pred_test)
-        y_observed_test.append(y_test)
-    
-    return pd.concat(y_prediction_train), pd.concat(y_observed_train), pd.concat(y_prediction_test), pd.concat(y_observed_test)
