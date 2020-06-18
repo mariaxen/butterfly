a2d0a3e2cf3cb9a22064e06fd231b105b7fa67ce
mgbckr <1241516+mgbckr@users.noreply.github.com>
2020-06-18 15:56:59 -0700

    Add data preparation code

--- src/butterfly/picturebook/album_preparation.py
+++ src/butterfly/picturebook/album_preparation.py
@@ -1,9 +1,18 @@
-# %%
+# %% enable auto reloading packages
+%load_ext autoreload
+%autoreload 2
+
+# %% imports
 import pandas as pd
 import pathlib
 from butterfly.deepinsight.album.album import AlbumTransformer
+import cuml.manifold
+import sklearn.manifold
+import matplotlib.pyplot as plt
+import pickle
+import os
 
-# %%
+# %% settings
 
 # data_path = pathlib.Path("/home/mgbckr/Documents/workspaces/nalab-butterfly")
 data_path = pathlib.Path("/home/mgbckr/mnt/nalab/workspaces/nalab-butterfly/data")
@@ -14,11 +23,28 @@ data_multiomics_training = pd.read_csv(data_path / "picturebook/multiomics_train
 data_multiomics_training.columns = pd.MultiIndex.from_tuples([
     ("meta", c[0]) if "Unnamed" in c[1] else c for c in data_multiomics_training.columns])
 
-
 # %%
-import cuml.manifold
+# %%time
+# album_transformer = AlbumTransformer(64, sklearn.manifold.TSNE())
+# album_transformer.fit(data_multiomics_training.immune_system.values)
 
-# %%
+# %% rapid AI album transformer
+%%time
 album_transformer = AlbumTransformer(64, cuml.manifold.TSNE())
+album_transformer.fit(data_multiomics_training.immune_system.values)
+
+# %%
+album = album_transformer.transform(data_multiomics_training.immune_system.values)
+print(album.shape)
+
+# %%
+plt.imshow(album[0,0,::])
+
+# %%
+out_path = data_path / "_interim/picturebook"
+os.makedirs(exist_ok=True)
+
+with open(out_path / "multiomics_training_album.pkl", "wb") as f:
+    pickle.dump(album, f)
 
 # %%
