14654ff1d80f64584e9191a7f5df425a009fa073
mgbckr <1241516+mgbckr@users.noreply.github.com>
2020-06-18 16:01:44 -0700

    Refine album prep script

--- src/butterfly/picturebook/album_preparation.py
+++ src/butterfly/picturebook/album_preparation.py
@@ -11,40 +11,39 @@ import sklearn.manifold
 import matplotlib.pyplot as plt
 import pickle
 import os
+import seaborn as sns
 
 # %% settings
 
 # data_path = pathlib.Path("/home/mgbckr/Documents/workspaces/nalab-butterfly")
 data_path = pathlib.Path("/home/mgbckr/mnt/nalab/workspaces/nalab-butterfly/data")
 
-# %%
+# %% load data / CSV
 
 data_multiomics_training = pd.read_csv(data_path / "picturebook/multiomics_training.csv.gz", index_col=0, header=[0,1])
 data_multiomics_training.columns = pd.MultiIndex.from_tuples([
     ("meta", c[0]) if "Unnamed" in c[1] else c for c in data_multiomics_training.columns])
 
-# %%
+# %% initialize regular album transformer
 # %%time
 # album_transformer = AlbumTransformer(64, sklearn.manifold.TSNE())
 # album_transformer.fit(data_multiomics_training.immune_system.values)
 
-# %% rapid AI album transformer
+# %% initialize rapid AI album transformer
 %%time
 album_transformer = AlbumTransformer(64, cuml.manifold.TSNE())
 album_transformer.fit(data_multiomics_training.immune_system.values)
 
-# %%
+# %% transform data
 album = album_transformer.transform(data_multiomics_training.immune_system.values)
 print(album.shape)
 
-# %%
-plt.imshow(album[0,0,::])
-
-# %%
+# %% write album to disk
 out_path = data_path / "_interim/picturebook"
-os.makedirs(exist_ok=True)
+os.makedirs(out_path, exist_ok=True)
 
 with open(out_path / "multiomics_training_album.pkl", "wb") as f:
     pickle.dump(album, f)
 
-# %%
+# %% visualize
+sns.heatmap(album[0,1,::])
